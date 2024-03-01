23b122685099b900df6d6fb2dcc6104c6e7b1cf2
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-03-01 11:02:54 +0100

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Łukasz Andrzejewski on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
